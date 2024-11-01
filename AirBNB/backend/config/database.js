68bf544e130111a05eab374253e65f15704798b2
Jason <jayz71189@gmail.com>
2024-11-01 16:40:39 -0400

    upload

---
+++ AirBNB/backend/config/database.js
@@ -0,0 +1,26 @@
+// backend/config/database.js
+const config = require("./index");
+
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "database_development",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
