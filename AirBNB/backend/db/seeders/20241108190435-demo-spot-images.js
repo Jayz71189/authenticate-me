4b6849a656a09741682b1b3503ed16af5d302c1d
Jason <jayz71189@gmail.com>
2024-11-18 12:48:03 -0500

    added reviews 3

--- AirBNB/backend/db/seeders/20241108190435-demo-spot-images.js
+++ AirBNB/backend/db/seeders/20241108190435-demo-spot-images.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   async up(queryInterface, Sequelize) {
     try {
-      await SpotImage.bulkCreate([
+      await SpotImage.bulkInsert([
         {
           spotId: 1, // Assuming the spot ID is 1
           url: "https://example.com/image1.jpg",
