7cb4d7a8914c47c0c675fdf98d0344351238dd9d
Jason <jayz71189@gmail.com>
2024-11-18 12:25:46 -0500

    added reviews 3

--- AirBNB/backend/db/models/user.js
+++ AirBNB/backend/db/models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Review, {
-        foreignKey: "ownerId",
+        foreignKey: "userId",
       });
       User.hasMany(models.Spot, { foreignKey: "ownerId" });
     }
