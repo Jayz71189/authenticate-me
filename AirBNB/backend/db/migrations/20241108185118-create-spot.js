2a83bb9f470950668e555eba8889b2a7b5a8c77f
Jason <jayz71189@gmail.com>
2024-11-24 22:53:01 -0500

    final

--- AirBNB/backend/db/migrations/20241108185118-create-spot.js
+++ AirBNB/backend/db/migrations/20241108185118-create-spot.js
@@ -54,7 +54,7 @@ module.exports = {
           allowNull: false,
         },
         price: {
-          type: Sequelize.NUMERIC,
+          type: Sequelize.DECIMAL,
           allowNull: false,
         },
         avgRating: {
